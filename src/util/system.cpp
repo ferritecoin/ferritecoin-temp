8536592066f06eaec18aa231cff86ec38fc6b62b
koh-gt <101822992+koh-gt@users.noreply.github.com>
2023-02-06 18:16:17 +0800

    ferrite.conf

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -71,7 +71,7 @@
 // Application startup time (used for uptime calculation)
 const int64_t nStartupTime = GetTime();
 
-const char * const BITCOIN_CONF_FILENAME = "litecoin.conf";
+const char * const BITCOIN_CONF_FILENAME = "ferrite.conf";
 const char * const BITCOIN_SETTINGS_FILENAME = "settings.json";
 
 ArgsManager gArgs;
